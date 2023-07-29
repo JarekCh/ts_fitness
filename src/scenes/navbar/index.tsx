a6a525f7f6568fd9210fc0f0221c3481e00d7ce3
Jaroslaw Charchula <charchula@gmail.com>
2023-07-29 23:43:45 +0200

    add mobile menu logic

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -7,18 +7,22 @@ import useMediaQuery from "@/hooks/useMadiaQuery";
 import ActionButton from "@/shared/ActionButton";
 
 type Props = {
+    isTopOfPage: boolean;
     selectedPage: SelectedPage;
     setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
     const flexBetween = "flex items-center justify-between";
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
     const isAboveMadiumScreens = useMediaQuery("(min-width: 1060px)");
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow";
 
     return (
         <nav>
-            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+            <div
+                className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}
+            >
                 <div className={`${flexBetween} mx-auto w-5/6`}>
                     <div className={`${flexBetween} w-full gap-16`}>
                         {/* Left side of navbar */}
@@ -50,9 +54,9 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                                 </div>
                                 <div className={`${flexBetween} gap-8`}>
                                     <p>Sign In</p>
-                                    <ActionButton
-                                        setSelectedPage={setSelectedPage}
-                                    >Become a Member</ActionButton>
+                                    <ActionButton setSelectedPage={setSelectedPage}>
+                                        Become a Member
+                                    </ActionButton>
                                 </div>
                             </div>
                         ) : (
@@ -66,6 +70,42 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                     </div>
                 </div>
             </div>
+
+            {/* MOBILE MENU MODAL */}
+            {!isAboveMadiumScreens && isMenuToggled && (
+                <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl ">
+                    {/* CLOSE ICON */}
+                    <div className="flex justify-end p-12">
+                        <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                            <XMarkIcon className="h-6 w-6 text-gray-400" />
+                        </button>
+                    </div>
+
+                    {/* MENU ITEMS */}
+                    <div className={`ml-[33%] flex flex-col gap-10 text-2xl`}>
+                        <Link
+                            page="Home"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Benefits"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Our Classes"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                            page="Contact Us"
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    </div>
+                </div>
+            )}
         </nav>
     );
 };
