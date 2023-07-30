72a6b30bf1d2d6a8fa1c621685616515434bacdd
Jaroslaw Charchula <charchula@gmail.com>
2023-07-30 23:36:07 +0200

    add partial form with safeties

--- src/scenes/contacUs/index.tsx
+++ src/scenes/contacUs/index.tsx
@@ -52,6 +52,86 @@ const ContactUs = ({ setSelectedPage }: Props) => {
                     </p>
                 </motion.div>
                 {/* FORM AND IMAGE */}
+                <div className="mt-10 justify-between gap-8 md:flex">
+                    <motion.div
+                        className="mt-10 basis-3/5 md:mt-0"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, y: 50 },
+                            visible: { opacity: 1, y: 0 },
+                        }}
+                    >
+                        <form
+                            target="_blank"
+                            onSubmit={onSubmit}
+                            action="https://formsubmit.co/got_@_formsubmit.co"
+                            method="POST"
+                        >
+                            <input
+                                className={inputStyles}
+                                type="text"
+                                placeholder="NAME"
+                                {...register("name", {
+                                    required: true,
+                                    maxLength: 100,
+                                })}
+                            />
+                            {errors.name && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.name.type === "required" && "This field is required."}
+                                    {errors.name.type === "maxLength" &&
+                                        "Max length is 100 char."}
+                                </p>
+                            )}
+
+                            <input
+                                className={inputStyles}
+                                type="text"
+                                placeholder="EMAIL"
+                                {...register("email", {
+                                    required: true,
+                                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                })}
+                            />
+                            {errors.email && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.email.type === "required" &&
+                                        "This field is required."}
+                                    {errors.email.type === "pattern" && "Invalid email address."}
+                                </p>
+                            )}
+
+                            <textarea
+                                className={inputStyles}
+                                placeholder="MESSAGE"
+                                rows={4}
+                                cols={50}
+                                {...register("message", {
+                                    required: true,
+                                    maxLength: 2000,
+                                })}
+                            />
+                            {errors.message && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.message.type === "required" &&
+                                        "This field is required."}
+                                    {errors.message.type === "maxLength" &&
+                                        "Max length is 2000 char."}
+                                </p>
+                            )}
+
+                            <button
+                                type="submit"
+                                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+                            >
+                                SUBMIT
+                            </button>
+                        </form>
+                    </motion.div>
+                </div>
             </motion.div>
         </section>
     )
