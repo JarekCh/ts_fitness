b3bda28fba6b2d92eb169ddeb9356a7770dddd37
Jaroslaw Charchula <charchula@gmail.com>
2023-07-30 20:49:05 +0200

    create benefit component add interface to shared types edit benefit index

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -1,11 +1,33 @@
 import HText from "@/shared/HText";
-import { SelectedPage } from "@/shared/types";
+import { BenefitType, SelectedPage } from "@/shared/types";
 import {
     HomeModernIcon,
     UserGroupIcon,
     AcademicCapIcon,
 } from "@heroicons/react/24/solid";
 import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+    {
+        icon: <HomeModernIcon className="h-6 w-6" />,
+        title: "State of the Art Facilities",
+        description:
+            "Neque adipiscing amet amet enim. Feugiat dolor enim fermentum in a in lectus pellentesque. Ullamcorper et.",
+    },
+    {
+        icon: <UserGroupIcon className="h-6 w-6" />,
+        title: "100's of Diverse Classes",
+        description:
+            "Eu ipsum id egestas risus tempus enim semper felis quis. Nec consectetur ac venenatis facilisi est. Eget ac turpis id.",
+    },
+    {
+        icon: <AcademicCapIcon className="h-6 w-6" />,
+        title: "Expert and Pro Trainers",
+        description:
+            "Fusce vestibulum aliquam ut cras. Nisl lectus egestas sapien nisl. Lacus at mi sit pellentesque. Congue parturient.",
+    },
+];
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
@@ -13,9 +35,7 @@ type Props = {
 
 const Benefits = ({ setSelectedPage }: Props) => {
     return (
-        <section
-            id="benefits"
-            className=" mx-auto min-h-full w-5/6 py-20 ">
+        <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
             <motion.div
                 onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
             >
@@ -31,14 +51,19 @@ const Benefits = ({ setSelectedPage }: Props) => {
 
                 {/* BENEFITS */}
                 <div className="mt-5 items-center justify-between gap-8 md:flex">
-                    {benefits.map((benefit) => (
-                        <Benefit />
+                    {benefits.map((benefit: BenefitType) => (
+                        <Benefit
+                            key={benefit.title}
+                            icon={benefit.icon}
+                            title={benefit.title}
+                            description={benefit.description}
+                            setSelectedPage={setSelectedPage}
+                        />
                     ))}
                 </div>
-
             </motion.div>
         </section>
-    )
-}
+    );
+};
 
-export default Benefits
+export default Benefits;
